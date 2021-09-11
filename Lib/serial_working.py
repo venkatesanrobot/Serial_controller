819459fc55b56f46595297bdb8228f3101a151e6
venkatesan <venkatesanrobotic@mail.com>
2021-09-11 16:43:17 +0530

    serial moudle complte pull to main code

--- Lib/serial_working.py
+++ Lib/serial_working.py
@@ -1,40 +1,40 @@
 import serial
 import serial.tools.list_ports
+from time import sleep
 
 
 class ComSerialCommunication:
 
     def __init__(self):
-        self.PORT=[]
+        self.PORT = [comport.device for comport in serial.tools.list_ports.comports()]
+        self.Serialdata = ""
+        self.enterclose = 1
+        self.ser_com = serial.Serial()
+        self.ser_com.baudrate = 9600
+        self.ser_com.bytesize = serial.EIGHTBITS
+        self.ser_com.parity = serial.PARITY_NONE
+        self.ser_com.stopbits = serial.STOPBITS_ONE
+        self.ser_com.timeout = 10
+        self.ser_com.write_timeout = 20
 
-    def com_config(self,com_port):
-        self.ser_com=serial.Serial()
-        self.ser_com.port=com_port
-        self.ser_com.baudrate=9600
-        self.ser_com.bytesize=serial.EIGHTBITS
-        self.ser_com.parity=serial.PARITY_NONE
-        self.ser_com.stopbits=serial.STOPBITS_ONE
-        return self.ser_com
-
-    def list_port(self):
-        for _ in serial.tools.list_ports.comports():
-            self.PORT.append(_.device)
-
-    def port_write(self,write_key):
-        self.list_port()
-        self.port_commuincation=self.com_config(self.PORT[0])
-        self.port_commuincation.dsrdtr=False
-        self.port_commuincation.open()
-        self.port_commuincation.write(str(write_key).encode("utf-8"))
-        self.port_commuincation.close()
-
+    def port_write(self, write_key):
+        self.ser_com.dsrdtr = False
+        self.ser_com.port = self.PORT[0]
+        self.ser_com.open()
+        self.ser_com.write(str(write_key).encode("utf-8"))
+        self.ser_com.close()
 
     def port_read(self):
-        pass
+        self.ser_com.port = self.PORT[0]
+        self.enterclose = 1
+        self.ser_com.open()
+        sleep(2)
+        self.Serialdata = self.ser_com.readline()
+        self.ser_com.close()
+        return self.Serialdata.decode("utf-8")
 
 
 
-
-data=ComSerialCommunication()
-#data.list_port()
-data.port_write("OFF")
+# data=ComSerialCommunication()
+# data.port_write("OFF")
+# print(data.port_read())
